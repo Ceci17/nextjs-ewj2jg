d6b16655d469948638908719a41dc5a44b3c773f
Ceci17 <ceci17sn@gmail.com>
2021-06-29 11:25:52 +0200

    1 file modified

--- components/GlobalFilter.js
+++ components/GlobalFilter.js
@@ -1,9 +1,18 @@
-export default function GlobalFilter({ filter, setFilter }) {
+import { useState } from 'react';
+import { useAsyncDebounce } from 'react-table';
+
+export default function GlobalFilter({ filter = '', setFilter }) {
+  const [value, setValue] = useState(filter);
+
+  const onChange = useAsyncDebounce(value => {
+    setFilter(value);
+  }, 1000);
+
   return (
     <div>
       Search:{' '}
       <input
-        value={filter || ''}
+        value={filter}
         onChange={e => setFilter(e.target.value)}
         placeholder="search table"
       />
